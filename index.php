1818aaa3f0be34cbdb16a091c4b7759f44b52249
knightdavieon <43941311+knightdavieon@users.noreply.github.com>
2018-11-08 14:28:19 +0800

    landing page

--- index.php
+++ index.php
@@ -6,20 +6,88 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Zambales Coastal Cleanup Trash Data">
     <meta name="author" content="John Dave Espinosa">
-    <link rel="icon" type="resources/admindesign/image/png" sizes="16x16" href="resources/images/ICCLogo.png">
+    <link rel="icon" type="resources/admindesign/image/png" sizes="16x16" href="resources/images/GADDSEAPLOGOV2.png">
     <title>GADDSEAP</title>
 
     <link rel="stylesheet" href="resources/bootstrap-4.0.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="resources/Font-Awesome-v5/css/fontawesome-all.min.css">
     <link rel="stylesheet" href="resources/customcss/navbar.css">
-    <script src="resources/bootstrap-4.0.0/dist/js/jquery-3.3.1.min.js"></script>
-    <script src="resources/bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
-    <script src="resources/bootstrap-4.0.0/dist/js/popper.min.js"></script>
+    <link rel="stylesheet" href="resources/customcss/carouselcss.css">
+    <script src="resources/dist/js/jquery-3.3.1.min.js"></script>
+    <script src="resources/dist/js/bootstrap.min.js"></script>
+    <script src="resources/dist/js/popper.min.js"></script>
 
     <style>
+      body, html {
+      height: 100%;
 
+      }
+      body{
+        background-color: #F7F7F7;
+      }
     </style>
 </head>
 <body>
   <?php include('navbar.php');?>
+
+  <div class="viewportt"><!-- for Responsive Viewport (Insert Carousel or other item for "cover" effect)  -->
+      <!-- Carousel container -->
+      <div id="kni-carousel" class="carousel fade-carousel slide" data-ride="carousel">
+
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+              <li data-target="#kni-carousel" data-slide-to="0" class="active"></li>
+              <li data-target="#kni-carousel" data-slide-to="1"></li>
+              <li data-target="#kni-carousel" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Content -->
+          <div class="carousel-inner" role="listbox">
+
+              <!-- Slide 1 -->
+              <div class="carousel-item active">
+                  <div class="carousel-caption">
+                      <hgroup>
+                          <h1>GOV. ATTY. AMOR D. DELOSO SCHOLARSHIP AND EDUCATIONAL ASSISTANCE PROGRAM</h1>
+                          <h3>Catch Phrase</h3>
+                          <!-- <a type="button" class="btn btn-lg btn-car" role="button" href="reservation/">BOOK NOW</a> -->
+                      </hgroup>
+                  </div>
+                  <div class="overlay"></div>
+                  <div class="slide-1"></div>
+              </div>
+
+              <!-- Slide 2 -->
+              <div class="carousel-item">
+                  <div class="carousel-caption">
+                      <hgroup>
+                          <h1>Some advertising</h1>
+                          <h3>Statement here</h3>
+                          <!-- <a type="button" class="btn btn-lg btn-car" role="button" href="reservation/">BOOK NOW</a> -->
+                      </hgroup>
+                  </div>
+                  <div class="overlay"></div>
+                  <div class="slide-2"></div>
+              </div>
+
+              <!-- Slide 3 -->
+              <div class="carousel-item">
+                  <div class="carousel-caption">
+                      <hgroup>
+                          <h1>Some advertising</h1>
+                          <h3>Statement here</h3>
+                          <!-- <a type="button" class="btn btn-lg btn-car" role="button" href="reservation/">BOOK NOW</a> -->
+                      </hgroup>
+                  </div>
+                  <div class="overlay"></div>
+                  <div class="slide-3"></div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  <!-- END OF CAROUSEL-->
+
+
+
 </body>
--- navbar.php
+++ navbar.php
@@ -1,27 +1,24 @@
 <nav class="navbar fixed-top navbar-light navbar-expand-md bg-faded justify-content-end nav-cust-bg1">
     <a href="index" class="navbar-logo"></a>
     <form action="reservation/">
-      <button type="submit" class="btn btn-bknow" >BOOK NOW</button>
+      <button type="submit" class="btn btn-bknow" >Contact Us</button>
 
     </form>
 
 </nav>
 <nav class="navbar navbar-light navbar-expand-md bg-faded justify-content-end nav-cust-bg2" id="fixme2">
-    
+
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="navbar-collapse collapse" id="collapsingNavbar3">
         <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
             <li class="nav-item">
-                <a class="nav-link" href="accommodation">Accommodation</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="amenities">Amenities</a>
+                <a class="nav-link" href="announcements">Announcement</a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="contact-us">Contact Us</a>
+                <a class="nav-link" href="gallery">Gallery</a>
             </li>
         </ul>
     </div>
