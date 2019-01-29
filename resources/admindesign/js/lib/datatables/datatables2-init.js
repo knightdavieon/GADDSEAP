2f586472a9b19cf1aa7fd3306569581ef0ff593f
knightdavieon <43941311+knightdavieon@users.noreply.github.com>
2019-01-29 09:33:17 +0800

    stage fix table

--- resources/admindesign/js/lib/datatables/datatables2-init.js
+++ resources/admindesign/js/lib/datatables/datatables2-init.js
@@ -1,28 +1,5 @@
 $(document).ready(function() {
     $('#myTable2').DataTable( {
-        serverSide: true,
-        ordering: false,
-        searching: false,
-        ajax: function ( data, callback, settings ) {
-            var out = [];
-
-            for ( var i=data.start, ien=data.start+data.length ; i<ien ; i++ ) {
-                out.push( [ i+'-1', i+'-2', i+'-3', i+'-4', i+'-5' ] );
-            }
-
-            setTimeout( function () {
-                callback( {
-                    draw: data.draw,
-                    data: out,
-                    recordsTotal: 5000000,
-                    recordsFiltered: 5000000
-                } );
-            }, 50 );
-        },
-        scrollY: 200,
-        scroller: {
-            loadingIndicator: true
-        },
-        stateSave: true
+        "order": [[ 5, "desc" ]]
     } );
 } );
